4ae2ffa0bb5885b569c0fb11b5f1bdf3bda6af97
MaciejNET <mderon07@gmail.com>
2023-09-18 19:41:01 +0200

    Remove cmake-build-debug

--- cmake-build-debug/CMakeFiles/PaintQt.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for PaintQt.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CMakeFiles/PaintQt_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PaintQt_autogen"
-  "PaintQt_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PaintQt_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
